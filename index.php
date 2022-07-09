65c19ebfb89e3607b52e23227eb8a0a45c497ece
krishna <bhattakrishna111@gmail.com>
2022-07-09 23:09:51 +0545

    frontend initial phase of building home page completed

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require('header.php');
+require('hero.php');
+require('hero_contact.php');
+require('about_us.php');
+require('header.php');
+require('header.php');
+require('header.php');
+?>
---
+++ our_team.php
@@ -0,0 +1,41 @@
+
+
+
+
+<section class="ftco-section ftco-no-pt">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12 col-lg-3 pr-md-4 pb-lg-0 pb-4">
+                <div class="heading-section ftco-animate text-center text-lg-left">
+                    <span class="subheading">Team &amp; Staff</span>
+                    <h2>Our Team</h2>
+                    <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country</p>
+                    <p><a href="#" class="btn btn-secondary">View All Staff</a></p>
+                </div>
+            </div>
+
+            <?php foreach ($team as $one) { ?>
+                <div class="col-md-4 col-lg-3 ftco-animate d-flex">
+                    <div class="staff">
+                        <div class="img-wrap d-flex align-items-stretch">
+                            <div class="img align-self-stretch" style="background-image: url(admin/images/<?= $one['team_image'] ?>)"></div>
+                        </div>
+                        <div class="text pt-3 px-3 pb-4 text-center">
+                            <h3><?= $one['name'] ?></h3>
+                            <span class="position mb-2"><?= $one['domain'] ?></span>
+                            <div class="faded">
+                                <ul class="ftco-social text-center">
+                                    <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"></span></a></li>
+                                    <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"></span></a></li>
+                                    <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-google"></span></a></li>
+                                    <li class="ftco-animate"><a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"></span></a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            <?php } ?>
+
+        </div>
+    </div>
+</section>
